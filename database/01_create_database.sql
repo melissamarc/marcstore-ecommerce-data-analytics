5466df4a1a3d240ab83a3f8f6d4b76e1f53a13d8
melissamarc <melmarc1moreira@gmail.com>
2026-09-05 16:19:06 -0300

    criando banco de dados e tabelas

---
+++ database/01_create_database.sql
@@ -0,0 +1,2 @@
+create database marcstore;
+use marcstore;
---
+++ database/02_create_tables.sql
@@ -0,0 +1,50 @@
+create table clientes(
+id_cliente int not null auto_increment,
+nome varchar(50) not null,
+email varchar(60),
+cidade varchar(30) default 'São Paulo' not null,
+estado varchar(30) default 'SP' not null,
+data_cadastro date,
+primary key(id_cliente)
+); 
+
+create table categorias(
+id_categoria int not null auto_increment,
+nome_categoria varchar(50),
+primary key(id_categoria)
+);
+
+create table produtos(
+id_produto int not null,
+id_categoria int not null,
+nome_produto varchar(50),
+preco decimal,
+custo decimal, 
+estoque int,	
+primary key(id_produto)
+);
+
+create table pedidos(
+id_pedido int not null auto_increment,
+id_cliente int not null, 
+data_pedido date, 
+status varchar(20),
+primary key(id_pedido)
+);
+
+create table itens_pedido(
+id_item int not null,
+id_pedido int not null,
+id_produto int not null,
+quantidade int,
+preco_unitario decimal,
+primary key(id_item)
+);
+
+create table pagamentos(
+id_pagamento int,
+id_pedido int not null,
+forma_pagamento varchar(40),
+status_pagamento varchar(20),
+primary key(id_pagamento)
+);
